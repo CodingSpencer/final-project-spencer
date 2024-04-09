2c3339dc5b267cd24009b126f2e6d11c87b18c4c
CodingSpencer <sashcraft121@gmail.com>
2024-04-09 17:53:56 -0600

    solution file created

--- stacks/Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
+++ stacks/Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e29a2105eb6de3cf10e678f72eece4ebc5d721f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef868d69aee6ccdf35566718cb7aebcaa6f3687d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- stacks/Problem/obj/Debug/net8.0/Problem.AssemblyInfo.cs
+++ stacks/Problem/obj/Debug/net8.0/Problem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8455e0b840953f9555fb29cd2cf87432e4495fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef868d69aee6ccdf35566718cb7aebcaa6f3687d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
